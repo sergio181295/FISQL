05aa9ab2b866c7970099cb9936c729ba5b4f52b3
Sergio Guillen <chejoguillen@gmail.com>
2017-08-20 00:59:00 -0600

    Gramatica DML
    Se agrego la gramatica para sentencias DML: insertar, actualizar, borrar, seleccionar

--- gramaticas/usql.java
+++ gramaticas/usql.java
@@ -24,6 +24,11 @@ valor -> valor + valor
         |! valor
         |( valor )
         |llamada
+        |CADENA
+        |NUMERO
+        |VERDADERO
+        |FALSO
+        |CARACTER
 
 llamada -> llamada PAR1 enviados PAR2 
       |ID
@@ -31,7 +36,7 @@ llamada -> llamada PAR1 enviados PAR2
 enviados -> enviados COMA valor
             |valor
 
-/** OBJETOS
+/** OBJETOS USQL
       BASE DE DATOS
       TABLAS
       OBJETO
@@ -48,53 +53,89 @@ enviados -> enviados COMA valor
             |alterar
             |eliminar
 
-//DDL CREAR
-      crear -> CREAR crearCosa
+      //DDL CREAR
+            crear -> CREAR crearCosa
 
-      crearCosa -> BASE_DATOS ID PUNTOYCOMA
-                  |TABLA ID PAR1 atrTabla PAR2 PUNTOYCOMA 
-                  |OBJETO ID PAR1 atrTabla PAR2 PUNTOYCOMA
-                  |PROCEDIMIENTO ID PAR1 atrTabla PAR2 LLA1 sentenciasDML LLA2
-                  |FUNCION ID PAR1 atrTabla PAR2 tipo LLA1 sentenciasDML LLA2
-                  |USUARIO ID COLOCAR PASSWORD IGUAL valor PUNTOYCOMA
-      
-      atrTabla -> atrTabla COMA tipo ID 
-                  |atrTabla complemento
-                  |tipo ID
+            crearCosa -> BASE_DATOS ID PUNTOYCOMA
+                        |TABLA ID PAR1 atrTabla PAR2 PUNTOYCOMA 
+                        |OBJETO ID PAR1 atrTabla PAR2 PUNTOYCOMA
+                        |PROCEDIMIENTO ID PAR1 atrTabla PAR2 LLA1 sentenciasDML LLA2
+                        |FUNCION ID PAR1 atrTabla PAR2 tipo LLA1 sentenciasDML LLA2
+                        |USUARIO ID COLOCAR PASSWORD IGUAL valor PUNTOYCOMA
+            
+            atrTabla -> atrTabla COMA tipo ID 
+                        |atrTabla complemento
+                        |tipo ID
 
-      complemento -> NULO 
-                  |NO_NULO
-                  |AUTOINCREMENTABLE
-                  |LLAVE_PRIMARIA
-                  |LLAVE_FORANEA REFERENCIA ID
-                  |UNICO
+            complemento -> NULO 
+                        |NO_NULO
+                        |AUTOINCREMENTABLE
+                        |LLAVE_PRIMARIA
+                        |LLAVE_FORANEA REFERENCIA ID
+                        |UNICO
 
-//DDL USAR 
-      usar -> USAR ID PUNTOYCOMA
+      //DDL USAR 
+            usar -> USAR ID PUNTOYCOMA
 
-//DDL ALTERAR
-      alterar -> ALTERAR alterarCosa
+      //DDL ALTERAR
+            alterar -> ALTERAR alterarCosa
 
-      alterarCosa -> TABLA ID alterarTabla PUNTOYCOMA
-                  |OBJETO ID alterarObjeto PUNTOYCOMA
-                  |USUARIO ID CAMBIAR PASSWORD IGUAL valor PUNTOYCOMA
+            alterarCosa -> TABLA ID alterarTabla PUNTOYCOMA
+                        |OBJETO ID alterarObjeto PUNTOYCOMA
+                        |USUARIO ID CAMBIAR PASSWORD IGUAL valor PUNTOYCOMA
 
-      alterarTabla -> AGREGAR PAR1 atrTabla PAR2
-                  |QUITAR enviados
+            alterarTabla -> AGREGAR PAR1 atrTabla PAR2
+                        |QUITAR enviados
 
-      alterarObjeto -> AGREGAR PAR1 atrTabla PAR2
-                  |QUITAR enviados
+            alterarObjeto -> AGREGAR PAR1 atrTabla PAR2
+                        |QUITAR enviados
 
-//DDL ELIMINAR
-      eliminar -> ELIMINAR eliminarCosa
+      //DDL ELIMINAR
+            eliminar -> ELIMINAR eliminarCosa
 
-      eliminarCosa -> TABLA ID PUNTOYCOMA
-                  |BASE_DATOS ID PUNTOYCOMA
-                  |OBJETO ID PUNTOYCOMA 
-                  |USUARIO ID PUNTOYCOMA
+            eliminarCosa -> TABLA ID PUNTOYCOMA
+                        |BASE_DATOS ID PUNTOYCOMA
+                        |OBJETO ID PUNTOYCOMA 
+                        |USUARIO ID PUNTOYCOMA
 
+/** SENTENCIAS DML */
+      sentenciasDML -> sentenciasDML dml
+                        |dml
 
-retorno -> RETORNO valor PUNTOYCOMA
+      dml -> insertar
+            |actualizar
+            |borrar
+            |seleccionar
+
+      //DML INSERTAR
+            insertar -> INSERTAR EN TABLA ID tipoInsertar PUNTOYCOMA
+
+            tipoInsertar -> tipoInsertar VALORES PAR1 enviados PAR2
+                        |PAR1 enviados PAR2 
+
+      //DML ACTUALIZAR
+            actualizar -> actualizar DONDE valor
+                        |actualizar PUNTOYCOMA
+                        |ACTUALIZAR TABLA ID PAR1 enviados PAR2 VALORES PAR1 enviados PAR2
+
+      //DML BORRAR
+            borrar -> borrar DONDE valor
+                  |borrar PUNTOYCOMA
+                  |BORRAR EN TABLA ID 
+
+      //DML SELECCIONAR
+            seleccionar -> seleccionar ORDENAR_POR ID tipoOrden
+                        |seleccionar DONDE valor
+                        |seleccionar PUNTOYCOMA
+                        |SELECCIONAR camposSeleccion DE enviados
+
+            tipoOrden -> ASC
+                        |DESC
+
+            camposSeleccion -> ASTERISCO
+                              |enviados
+
+etorno -> RETORNO valor PUNTOYCOMA
 
 
 
